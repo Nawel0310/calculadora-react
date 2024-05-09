9cadfc455ade3c06186f95aa169a085ad609ef7e
Nawel <nbarbaro@frba.utn.edu.ar>
2024-05-09 17:42:55 -0300

    Agregados comentarios importantes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Calculadora } from './Calculadora';
 
+//Archivos de animación que no cargaron, revisar posteriormente.
 //import "./assets/css/aos.min.css";
 //import "./assets/js/aos.min.js";
 //import "./assets/js/bs-init.js";
 
+//Archivos de estilos y demás. Se cargan directamente en el HTML de la aplicación.
 //import "./assets/bootstrap/css/bootstrap.min.css"; 
 //import "./assets/bootstrap/js/bootstrap.min.js";
 //import "./assets/css/styles.css";
